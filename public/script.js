3d7f0df6dffda2eb7af70c2731b1bbaba534fbbc
derrick-ng <ng.derrick1@hotmail.com>
2025-09-07 03:16:54 -0700

    Add javascript to grab the user input

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.getElementById("scrape-button").addEventListener("click", () => {
+  const userNotes = document.getElementById("notes").value;
+  console.log("user notes:", userNotes);
+});
